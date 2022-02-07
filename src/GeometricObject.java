b3190960320f2722902a937fb920029dd46efd0a
KJ <tongkj2000@gmail.com>
2022-02-07 14:58:51 +0800

    Initial Commit

---
+++ src/GeometricObject.java
@@ -0,0 +1,45 @@
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class GeometricObject {
+    private String color = "white";
+    private Boolean filled = Boolean.FALSE;
+    private java.util.Date dateCreated = new Date();
+
+    public GeometricObject() {
+    }
+
+    public GeometricObject(String color, Boolean filled) {
+        this.color = color;
+        this.filled = filled;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public Boolean isFilled() {
+        return filled;
+    }
+
+    public void setFilled(Boolean filled) {
+        this.filled = filled;
+    }
+
+    public Date getDateCreated() {
+        return dateCreated;
+    }
+
+    public void setDateCreated(Date dateCreated) {
+        this.dateCreated = dateCreated;
+    }
+
+    public String toString(){
+        SimpleDateFormat dateFormat = new SimpleDateFormat("EEE MMM dd HH:mm:ss");
+        return "created on " + dateFormat.format(dateCreated) + "\nColor: " + color + " and filled: " + filled;
+    }
+}
